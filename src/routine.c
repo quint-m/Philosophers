b186f618b5d60b3385c3e5e8d40a3dca8d4d0b23
Quinten Mennen <qmennen@student.codam.nl>
2025-03-18 18:10:37 +0100

    chore (doc): add small todo

--- src/routine.c
+++ src/routine.c
@@ -6,7 +6,7 @@
 /*   By: qmennen <qmennen@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 18:04:57 by qmennen           #+#    #+#             */
-/*   Updated: 2025/03/13 18:46:41 by qmennen          ###   ########.fr       */
+/*   Updated: 2025/03/18 17:57:58 by qmennen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ void	p_sleep(t_philosopher *philo)
 
 void	p_eat(t_philosopher *philo)
 {
+	// Make sure we prevent deadlock by not always taking right fork first
 	pthread_mutex_lock(philo->l_fork);
 	info(philo, "has taken a fork");
 	pthread_mutex_lock(philo->r_fork);
