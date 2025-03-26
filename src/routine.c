42a414796b0f1b07e3f5e307f69361d381a72119
Quinten Mennen <qmennen@student.codam.nl>
2025-03-26 15:50:07 +0100

    chore (norm): routine.c

--- src/routine.c
+++ src/routine.c
@@ -6,29 +6,16 @@
 /*   By: qmennen <qmennen@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 18:04:57 by qmennen           #+#    #+#             */
-/*   Updated: 2025/03/25 17:32:15 by qmennen          ###   ########.fr       */
+/*   Updated: 2025/03/26 15:49:57 by qmennen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 #include <pthread.h>
 
-void	p_sleep(t_philosopher *philo)
+static void	pick_fork(int even, t_philosopher *philo)
 {
-	info(philo, "is sleeping");
-	acc_usleep(philo->tts);
-}
-
-void	p_eat(t_philosopher *philo)
-{
-	int	side;
-	if (philo->r_fork == NULL || philo->l_fork == NULL)
-	{
-		acc_usleep(philo->ttd);
-		return ;
-	}
-	// Make sure we prevent deadlock by not always taking right fork first
-	if (philo->p_num % 2 == 0)
+	if (even)
 	{
 		pthread_mutex_lock(philo->r_fork);
 		info(philo, "has taken a fork");
@@ -40,6 +27,22 @@ void	p_eat(t_philosopher *philo)
 		info(philo, "has taken a fork");
 		pthread_mutex_lock(philo->r_fork);
 	}
+}
+
+void	p_sleep(t_philosopher *philo)
+{
+	info(philo, "is sleeping");
+	acc_usleep(philo->tts);
+}
+
+void	p_eat(t_philosopher *philo)
+{
+	if (philo->r_fork == NULL || philo->l_fork == NULL)
+	{
+		acc_usleep(philo->ttd);
+		return ;
+	}
+	pick_fork(philo->p_num % 2 == 0, philo);
 	info(philo, "is eating");
 	pthread_mutex_lock(philo->eat_mutex);
 	philo->last_meal = get_time();
